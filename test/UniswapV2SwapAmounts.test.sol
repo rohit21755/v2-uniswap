1ffcbcacb8b7a9080c1eed936a4690191e9ed82f
rohit21755 <rohit.k09022002@gmail.com>
2025-03-26 09:28:57 +0530

    getAmountsOut

---
+++ test/UniswapV2SwapAmounts.test.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {IWETH} from "../src/interfaces/IWETH.sol";
+import {IERC20} from "../src/interfaces/IERC20.sol";
+import {IUniswapV2Router02} from "../src/interfaces/uniswap-v2/IUniswapV2Router02.sol";
+import { DAI,WETH, MKR, UNISWAP_V2_ROUTER_02 } from "../src/constants.sol";
+
+contract UniswapV2SwapAmountsTest is Test {
+    IWETH weth = IWETH(WETH);
+    IERC20 dai = IERC20(DAI);
+    IERC20 mkr = IERC20(MKR);
+    IUniswapV2Router02 private router = IUniswapV2Router02(UNISWAP_V2_ROUTER_02);
+    function test_getAmountsOut() public {
+        console.log("weth");
+        address[] memory path = new address[](3);
+        path[0] = address(weth);
+        path[1] = address(dai);
+        path[2] = address(mkr);
+        uint256 amountIn =1e18;
+
+        uint[] memory amounts = router.getAmountsOut(amountIn, path);
+        console.log(amounts.length);
+        console.log("WETH",amounts[0]);
+        console.log("DAI",amounts[1]);
+        console.log("MKR",amounts[2]);
+    }
+}
